c617d7cd465fb4a34723e63ad9ab91f9477f9e06
Guillaume Filipe <filipe.guillaume@gmail.com>
2020-11-08 20:06:34 +0100

    Add title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonText } from '@ionic/react';
 import TodayWeather from '../components/TodayWeather';
 
 import './Home.css';
@@ -8,6 +8,9 @@ const Home = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
+        <IonText className="main-title">
+          <h1>Weather</h1>
+        </IonText>
         <TodayWeather />
       </IonContent>
     </IonPage>
